16d01113c5b1f97db67d994fe6ae4d0b771e1299
MattScuteri <mttscuteri@gmail.com>
2018-04-24 21:55:49 -0400

    initial commit @todo: breakout controller in sep file

---
+++ app.js
@@ -0,0 +1,62 @@
+const app = angular.module('Piano', [])
+.controller('NotesCtrl', function($scope) {
+	var vm = this;
+	vm.title = 'Mozart bitch '
+	/*vm.note = [{
+		note: 'C',
+		keyStroke: 'A',
+		black: false
+	}, {
+		note: 'C#',
+		keyStroke: 'W',
+		black: true
+	}, {
+		note: 'D',
+		keyStroke: 'S',
+		black: false
+	}, {
+		note: 'D#',
+		keyStroke: 'E',
+		black: true
+	}, {
+		note: 'E',
+		keyStroke: 'D',
+		black: false
+	}, {
+		note: 'F',
+		keyStroke: 'F',
+		black: false
+	}, {
+		note: 'F#',
+		keyStroke: 'T',
+		black: true
+	}, {
+		note: 'G',
+		keyStroke: 'G',
+		black: false
+	}, {
+		note: 'G#',
+		keyStroke: 'Y',
+		black: true
+	}, {
+		note: 'A',
+		keyStroke: 'H',
+		black: false
+	}, {
+		note: 'A#',
+		keyStroke: 'U',
+		black: true
+	}, {
+		note: 'B',
+		keyStroke: 'J',
+		black: false
+	}, {
+		note: 'C2',
+		keyStroke: 'K',
+		black: false
+	}, {
+		note: 'C#2',
+		keyStroke: 'O',
+		black: true
+	}];*/
+});
