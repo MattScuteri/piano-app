81a8c947b36cab8aca489db22010d8a8562102c9
MattScuteri <mttscuteri@gmail.com>
2018-04-25 02:17:47 -0400

    app.js edits

--- app.js
+++ app.js
@@ -1,62 +1,49 @@
 const app = angular.module('Piano', [])
 .controller('NotesCtrl', function($scope) {
 	var vm = this;
-	vm.title = 'Mozart bitch '
-	/*vm.note = [{
+	vm.title = 'Keyboard Piano'
+	vm.whiteNote = [{
 		note: 'C',
 		keyStroke: 'A',
-		black: false
-	}, {
-		note: 'C#',
-		keyStroke: 'W',
-		black: true
 	}, {
 		note: 'D',
 		keyStroke: 'S',
-		black: false
-	}, {
-		note: 'D#',
-		keyStroke: 'E',
-		black: true
 	}, {
 		note: 'E',
 		keyStroke: 'D',
-		black: false
 	}, {
 		note: 'F',
 		keyStroke: 'F',
-		black: false
-	}, {
-		note: 'F#',
-		keyStroke: 'T',
-		black: true
 	}, {
 		note: 'G',
 		keyStroke: 'G',
-		black: false
-	}, {
-		note: 'G#',
-		keyStroke: 'Y',
-		black: true
 	}, {
 		note: 'A',
 		keyStroke: 'H',
-		black: false
-	}, {
-		note: 'A#',
-		keyStroke: 'U',
-		black: true
 	}, {
 		note: 'B',
 		keyStroke: 'J',
-		black: false
 	}, {
 		note: 'C2',
 		keyStroke: 'K',
-		black: false
+	}];
+	vm.blackNote = [{
+		note: 'C#',
+		keyStroke: 'W',
+	}, {
+		note: 'D#',
+		keyStroke: 'E',
+	}, {
+		note: 'F#',
+		keyStroke: 'T',
+	}, {
+		note: 'G#',
+		keyStroke: 'Y',
+	}, {
+		note: 'A#',
+		keyStroke: 'U',
 	}, {
 		note: 'C#2',
 		keyStroke: 'O',
-		black: true
-	}];*/
+	}];
 });
