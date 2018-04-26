867d8b87cde08a157039b7889afd0a68d06ea836
MattScuteri <mttscuteri@gmail.com>
2018-04-25 21:29:26 -0400

    keyboard logic added

--- app.js
+++ app.js
@@ -2,48 +2,4 @@ const app = angular.module('Piano', [])
 .controller('NotesCtrl', function($scope) {
 	var vm = this;
 	vm.title = 'Keyboard Piano'
-	vm.whiteNote = [{
-		note: 'C',
-		keyStroke: 'A',
-	}, {
-		note: 'D',
-		keyStroke: 'S',
-	}, {
-		note: 'E',
-		keyStroke: 'D',
-	}, {
-		note: 'F',
-		keyStroke: 'F',
-	}, {
-		note: 'G',
-		keyStroke: 'G',
-	}, {
-		note: 'A',
-		keyStroke: 'H',
-	}, {
-		note: 'B',
-		keyStroke: 'J',
-	}, {
-		note: 'C2',
-		keyStroke: 'K',
-	}];
-	vm.blackNote = [{
-		note: 'C#',
-		keyStroke: 'W',
-	}, {
-		note: 'D#',
-		keyStroke: 'E',
-	}, {
-		note: 'F#',
-		keyStroke: 'T',
-	}, {
-		note: 'G#',
-		keyStroke: 'Y',
-	}, {
-		note: 'A#',
-		keyStroke: 'U',
-	}, {
-		note: 'C#2',
-		keyStroke: 'O',
-	}];
 });
